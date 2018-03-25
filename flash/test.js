906d6a8106e1c5addaaf3643d7e851b7fa9b47d1
unknown <vishnumd22@gmail.com>
2018-03-25 02:11:48 -0700

    meh

--- flash/test.js
+++ flash/test.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
-
+global.Tether = require('tether');
 let win;
 
 function createWindow(){
