03047f586312ebd358967a50dc6bc176f9e15e4b
unknown <vishnumd22@gmail.com>
2018-04-05 09:48:18 -0700

    woah adioli

--- flash/test.js
+++ flash/test.js
@@ -1,19 +1,19 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
-global.Tether = require('tether');
+
 let win;
 
 function createWindow(){
     win = new BrowserWindow({width:800, height:600});
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'test.html'),
+        pathname: path.join(__dirname, 'main.html'),
         protocol: 'file:',
         slashes: true
     }));
 
-   // win.webContents.openDevTools();
+    win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null
