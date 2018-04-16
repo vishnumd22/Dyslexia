466845c1885120cd94502ba4e8a740f633d5cf7f
unknown <vishnumd22@gmail.com>
2018-04-15 23:17:45 -0700

    vim

--- flash/hangman/hangman.js
+++ flash/hangman/hangman.js
@@ -245,17 +245,17 @@
   };
 
   //var wordList = ["chrome", "firefox", "codepen", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "banner", "browser", "screen", "mobile", "footer", "header", "typography", "responsive", "programmer", "css", "border", "compass", "grunt", "pixel", "document", "object", "ruby", "modernizr", "bootstrap", "python", "php", "pattern", "ajax", "node", "element", "android", "application", "adobe", "apple", "google", "microsoft", "bookmark", "internet", "icon", "svg", "background", "property", "syntax", "flash", "html", "font", "blog", "network", "server", "content", "database", "socket", "function", "variable", "link", "apache", "query", "proxy", "backbone", "angular", "email", "underscore", "cloud"];
-  var wordList;
+  var wordList = [];
  
         const loadJsonFile = require('load-json-file');
         
-        loadJsonFile('hangman/hangman.json').then(json => {
-            //words = json;
-            //console.log(words[1]);
+        loadJsonFile('hangman/words.json').then(json => {
             
-               wordList=json;
-
+            console.log(json);
             
+            wordList.push(json); 
+  
+            console.log(wordList);
         });
   Hangman.init(wordList);
   
