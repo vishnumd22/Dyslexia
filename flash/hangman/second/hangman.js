c12f65fd5ff0485069d49a776936af39eea3b3bb
unknown <vishnumd22@gmail.com>
2018-04-16 02:56:21 -0700

    ippo

--- flash/hangman/second/hangman.js
+++ flash/hangman/second/hangman.js
@@ -243,20 +243,11 @@
     }
   
   };
-
-  //var wordList = ["chrome", "firefox", "codepen", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "banner", "browser", "screen", "mobile", "footer", "header", "typography", "responsive", "programmer", "css", "border", "compass", "grunt", "pixel", "document", "object", "ruby", "modernizr", "bootstrap", "python", "php", "pattern", "ajax", "node", "element", "android", "application", "adobe", "apple", "google", "microsoft", "bookmark", "internet", "icon", "svg", "background", "property", "syntax", "flash", "html", "font", "blog", "network", "server", "content", "database", "socket", "function", "variable", "link", "apache", "query", "proxy", "backbone", "angular", "email", "underscore", "cloud"];
-  var wordList = [];
  
-        const loadJsonFile = require('load-json-file');
-        
-        loadJsonFile('hangman/words.json').then(json => {
-            
-            console.log(json);
-            
-            wordList.push(json); 
-  
-            console.log(wordList);
-        });
+ 
+  var wordList = ["right","sing","sit","sleep","tell","their","these","those","upon","us","use","very"];
+ 
+
   Hangman.init(wordList);
   
 })(jQuery, window);
